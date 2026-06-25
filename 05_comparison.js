94f588fda1900709c58218480c5ecc637a3aef42
vedant <vedantsanap0002@gmail.com>
2026-06-25 14:10:38 +0530

    use of ===

---
+++ 05_comparison.js
@@ -0,0 +1,6 @@
+console.log("2">1)
+console.log("2"===1)
+
+
+
+// "===" is used for strict comparison, which means it checks both the value and the type of the operands. In this case, "2" is a string and 1 is a number, so they are not strictly equal.
