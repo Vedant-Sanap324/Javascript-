42f87f867d4db56ddfda2cb2c82e60464707645d
vedant <vedantsanap0002@gmail.com>
2026-06-27 22:08:24 +0530

    learnt about imp datatypes

---
+++ datatypes_summary.js
@@ -0,0 +1,7 @@
+// 7 types of datatypes in javascript
+//there is no special datatype for integers, floats, or doubles. All numbers are represented as floating point values.
+//special type of datatype called symbols
+//eg:
+const id = Symbol('123')
+const anotherid = Symbol('123')
+console.log(id === anotherid) // false
