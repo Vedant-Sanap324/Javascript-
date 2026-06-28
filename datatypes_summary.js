6b83ef0b727825b5c5c7deca2735aee04ff65b51
vedant <vedantsanap0002@gmail.com>
2026-06-28 14:11:08 +0530

    hfhfh

--- datatypes_summary.js
+++ datatypes_summary.js
@@ -14,6 +14,6 @@ let object = {
 const myfuction = function()
 { console.log('hello world')}
 
-myfuction() // hello world
+myfuction() // hello world-->function call 
 
 //null ka datatype hmesha object hota h
