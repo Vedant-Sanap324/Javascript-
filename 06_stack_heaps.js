f005b8f0d93edc845e1ceeeceae55a9b19269bd6
vedant <vedantsanap0002@gmail.com>
2026-06-28 14:30:46 +0530

    heap and stack

---
+++ 06_stack_heaps.js
@@ -0,0 +1,4 @@
+//two types of memory storage in javascript are stacka and heap
+//stack is used for static memory allocation and heap is used for dynamic memory allocation
+//stack-->primitive
+//heap-->non-primitive 
