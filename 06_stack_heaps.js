34c0c5cde80dcc56bae074cb14ce87468ca7a9ee
vedant <vedantsanap0002@gmail.com>
2026-06-28 14:36:35 +0530

    explanation

--- 06_stack_heaps.js
+++ 06_stack_heaps.js
@@ -2,3 +2,16 @@
 //stack is used for static memory allocation and heap is used for dynamic memory allocation
 //stack-->primitive
 //heap-->non-primitive 
+let myname="vedant"
+anothername=myname
+console.log(myname)
+console.log(anothername)
+let obj1={
+    email:"vedant@gmail.com"
+}
+let obj2=obj1
+console.log(obj1)
+console.log(obj2)
+obj2.email="vvvv@gmail.com"
+console.log(obj1)
+console.log(obj2)
