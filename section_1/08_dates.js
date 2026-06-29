534b837e6dd6713a14bb50fe731a73715ddfb824
vedant <vedantsanap0002@gmail.com>
2026-06-29 22:02:55 +0530

    imp concepts of strings and dates

---
+++ section_1/08_dates.js
@@ -0,0 +1,4 @@
+let mydate= new Date()
+console.log(mydate.toString())
+
+//and many more methods are there not compulsory to use all
---
+++ section_1/nums-maths.js
@@ -0,0 +1,10 @@
+const score=400
+console.log(score)
+//toFixed() is used to specify the number of digits after decimal point
+const score1=400.2345
+console.log(score1.toFixed(2))
+//++++++++++++++++++++++Maths++++++++++++++++++++++++
+console.log(Math.abs(-4))
+console.log(Math.floor(4.9))
+console.log(Math.round(4.2))
+console.log(Math.sqrt(16))
