154d1f7035bca13bc012624b3575d58e71323184
vedant <vedantsanap0002@gmail.com>
2026-06-30 15:57:21 +0530

    arrays

---
+++ section_2/arrays.js
@@ -0,0 +1,13 @@
+//everything as same as python
+const arr =[1,2,3,4,5]
+
+console.log(arr)
+
+//array methods
+arr.push(6)//pushes the element at the end of array
+arr.pop()//removes the last element of array
+arr.shift()//removes the first element of array
+arr.unshift(0)//adds the element at the start of array (0 in this case) and removes the first element of array
+
+console.log(arr)
+console.log(arr.includes(3))//checks if the element is present in the array or not
