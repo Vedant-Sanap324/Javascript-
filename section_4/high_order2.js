1adf90e5bf3d4e8c5588e0cbd9edf6361e73a132
vedant <vedantsanap0002@gmail.com>
2026-07-29 23:29:32 +0530

    more about loops

---
+++ section_4/high_order2.js
@@ -0,0 +1,11 @@
+const coding=["js","cpp","java","python","c"]
+coding.forEach( function (val){
+//    console.log(val);
+})             //this is one of example of high order thing
+
+
+//using arrow function
+
+coding.forEach( (item)=>{
+    console.log(item);
+})
